a2abd66dba934577f87a7e7a7f655964416a2e63
ImaniMusafiri88 <imanimusafiri08@gmail.com>
2026-04-13 12:32:37 +0200

    updates urls.py add listing page url path

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -21,4 +21,5 @@ from listings import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.homepage, name='home'),
+    path('listings/', views.listings, name='listings'),
 ]

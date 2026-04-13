0cf3679aa391eebf83f34f8d54c9f56ac94d2316
ImaniMusafiri88 <imanimusafiri08@gmail.com>
2026-04-13 12:56:32 +0200

    updates merchex/listings/templates/base.html merchex/listings/static/css/style.css add nav_bar style

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from listings.models import Band
 
 
 def homepage(request):
     '''View function for the homepage of the listings app.'''
     return render(request, 'listings/home.html')
+
+
+def listings(request):
+    '''View function for the listings page.'''
+    bands = Band.objects.all()
+    context = {
+        'bands': bands,
+    }
+    return render(request, 'listings/listings.html', context)

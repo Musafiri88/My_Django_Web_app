f477120e461dcfff2c9edd09876eaf1f3bb96f6a
ImaniMusafiri88 <imanimusafiri08@gmail.com>
2026-04-13 12:23:59 +0200

    updates models.py create models

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Band(models.Model):
+    '''Model representing a band.'''
+    name = models.CharField(max_length=200)
+    genre = models.CharField(max_length=50)
+    biography = models.TextField()
+    year_formed = models.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)])
+    active = models.BooleanField(default=True)
+    official_homepage = models.URLField()
